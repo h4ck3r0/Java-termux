5ddc9b8ebd5b2897203d6d91b3be637de9f8e7db
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2022-06-20 00:20:56 +0530

    Create java.sh

---
+++ java.sh
@@ -0,0 +1,28 @@
+#!$PREFIX/bin/sh
+echo '
+
+       ██  █████  ██    ██  █████  
+       ██ ██   ██ ██    ██ ██   ██ 
+       ██ ███████ ██    ██ ███████ 
+  ██   ██ ██   ██  ██  ██  ██   ██ 
+   █████  ██   ██   ████   ██   ██ 
+       github.com/h4ck3r0/Java-termux 
+                             Openjdk8
+         
+  ' | lolcat
+
+                               
+cd $HOME/Java-termux
+cp -r jdk8 $PREFIX/share/
+
+cat << 'EOL' >> $PREFIX/bin/java
+
+#!/usr/bin/bash
+unset LD_PRELOAD
+export JAVA_HOME="$PREFIX/share/jdk8"
+export LIB_DIR="\$PREFIX/share/glib"
+export LD_LIBRARY_PATH="\$LIB_DIR"
+exec proot -0 \$JAVA_HOME/bin/java "\$@"
+
+EOL
+
